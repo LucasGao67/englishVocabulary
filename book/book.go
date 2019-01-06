56a3d399d8dbf0c3b7e703f4458316bcbefacf98
gaoke <ke.gao@horizon.ai>
2019-01-06 19:26:09 +0800

    compleate tmpl 2 latex

---
+++ book/book.go
@@ -0,0 +1,13 @@
+package book
+
+import (
+	"github.com/LucasGao67/englishVocabulary/book/entity"
+	"github.com/sirupsen/logrus"
+	"io"
+)
+
+func Generate(wr io.Writer, word *entity.Word) {
+	if err := tpl.Execute(wr, word); err != nil {
+		logrus.Error(err)
+	}
+}
---
+++ book/tmpl.go
@@ -0,0 +1,37 @@
+package book
+
+import "text/template"
+
+
+const section = `
+\section*{ {{- .Name -}} }
+{\large \color{blue} {{range .Forms }} {{.}} {{end}} }
+\subsection*{Explain}
+\begin{enumerate}
+{{ range .Contents -}}
+\item {{.Type}} \\
+{{ range .Explain -}}
+{{if .Imported}}\textbf{ {{- .Content -}} } {{else}}{{.Content}} {{end}}
+{{- end}}
+\textit{
+	\begin{itemize}
+	{{ range .ExampleSentences}}\item {{.}}
+	{{end -}}
+	\end{itemize}
+}
+{{end}}
+\end{enumerate}
+{{ if .ShowExample }}
+\subsection*{Example}
+\begin{enumerate}
+{{ range .ExampleSentences }}\item {{.}}
+{{end -}}
+\end{enumerate}
+{{end}}
+`
+
+var tpl = &template.Template{}
+
+func init() {
+	tpl = template.Must(tpl.New("section").Parse(section))
+}
