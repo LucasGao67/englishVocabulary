5b5a4d5c0ba6d6aedc49e756544314e8e1854bb3
gaoke <ke.gao@horizon.ai>
2019-01-07 14:31:45 +0800

    简单调整

--- capture/capture.go
+++ capture/capture.go
@@ -1,16 +1,12 @@
 package capture
 
 import (
-	"fmt"
 	"github.com/LucasGao67/englishVocabulary/book"
 	"github.com/LucasGao67/englishVocabulary/book/entity"
 	"github.com/antchfx/htmlquery"
-	"golang.org/x/net/html"
-	"gopkg.in/xmlpath.v2"
 	"io/ioutil"
 	"net/http"
 	"os"
-	"strings"
 )
 
 var baseUrl = "https://www.collinsdictionary.com/dictionary/english/"
@@ -20,7 +16,9 @@ func captureUrl(word string) string {
 	if err != nil {
 
 	}
-	defer resp.Body.Close()
+	defer func() {
+
+	}()
 	data, err2 := ioutil.ReadAll(resp.Body)
 	if err2 != nil {
 
@@ -28,58 +26,21 @@ func captureUrl(word string) string {
 	return string(data)
 }
 
-func parse(html string) *entity.Word {
-	path, _ := xmlpath.Compile("//*[@id='challenge__1']/div[3]/span")
-
-	root, err := xmlpath.Parse(strings.NewReader(html))
-	if err != nil {
-		//log.Fatal(err)
-	}
-	if value, ok := path.String(root); ok {
-		fmt.Println("Found:", value)
-	}
-	return nil
-}
-
-func parse1(word string) {
-	doc, err := htmlquery.LoadURL(baseUrl + word)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, n := range htmlquery.Find(doc, "//*[@id='challenge__1']/div[3]/span/span[@class='orth']") {
-		//fmt.Println(n)
-		fmt.Println(strings.Trim(htmlquery.InnerText(n), " "))
-	}
-	//htmlquery.Find(n, "")
-}
-
-func parseFrom(node *html.Node, word string) [] string {
-	expr := fmt.Sprintf("//*[@id='%s__1']/div[3]/span/span[@class='orth']", word)
-	var list []string
-	for _, n := range htmlquery.Find(node, expr) {
-		//fmt.Println(n)
-		content := strings.Trim(htmlquery.InnerText(n), " ")
-		list = append(list, content)
-		fmt.Println(content)
-	}
-	return list
-}
-
-func parseContents(node *html.Node, word string) {
-
-}
-
 func Convert(word string) {
 	//parse1(word)
-	//doc, _ := htmlquery.LoadURL(baseUrl + word)
-	doc, _ := htmlquery.Parse(strings.NewReader(file))
+	doc, _ := htmlquery.LoadURL(baseUrl + word)
+	//doc, _ := htmlquery.Parse(strings.NewReader(file))
 	w := &entity.Word{}
 	w.Name = word
 	w.SetExample(doc).SetForms(doc).SetContent(doc)
 
 	w.ShowExample = true
-	f, _ := os.Create("generate/" + word + ".txt")
+	f, _ := os.Create("doc/latex/all/" + word + ".txt")
 	defer f.Close()
 	book.Generate(f, w)
+
+	w.ShowExample = false
+	f1, _ := os.Create("doc/latex/tiny/" + word + ".txt")
+	defer f1.Close()
+	book.Generate(f1, w)
 }
--- main.go
+++ main.go
@@ -3,42 +3,19 @@ package main
 import "github.com/LucasGao67/englishVocabulary/capture"
 
 func main() {
-	//word := &entity.Word{
-	//	Name:        "challenge",
-	//	Forms:       []string{"change", "changing"},
-	//	ShowExample: true,
-	//	Contents: []*entity.Content{
-	//		{
-	//			Type: "adjective",
-	//			Explain: []*entity.Slice{
-	//				{
-	//					Content:  "A",
-	//					Imported: false,
-	//				},
-	//				{
-	//					Content:  "challenge",
-	//					Imported: true,
-	//				},
-	//				{
-	//					Content:  "is something new and difficult which requires great effort and determination.",
-	//					Imported: false,
-	//				},
-	//			},
-	//			ExampleSentences: []string{
-	//				"The challenge is to make it taste good.",
-	//				"This may feel tricky because it could also challenge your relationship with your husband.",
-	//			},
-	//		},
-	//	},
-	//	ExampleSentences: []string{
-	//		"The challenge is to make it taste good.",
-	//		"This may feel tricky because it could also challenge your relationship with your husband.",
-	//	},
-	//}
-	//f, _ := os.Create(word.Name + ".txt")
-	////w:= bufio.NewWriter(f)
-	//book.Generate(f, word)
 
-	capture.Convert("challenge")
+	capture.Convert("away")
 	//capture.Convert("test")
+
+	//file, _ := os.Open("word1.txt")
+	//defer file.Close()
+	//
+	//scanner := bufio.NewScanner(file)
+	//for scanner.Scan() {
+	//	word := scanner.Text()
+	//	fmt.Println(word)
+	//	capture.Convert(word)
+	//	time.Sleep(300 * time.Millisecond)
+	//	//fmt.Println(scanner.Text())
+	//}
 }
